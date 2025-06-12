83e6c7ce50736f7243f6ef1c86ab2c032f90b21d
unknown <sarookola@yahoo.com>
2025-06-12 16:33:44 +0330

    2nd version , ...

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
+
+bg_image = cv2.imread("1404.jpg")
 cap = cv2.VideoCapture(0)
 
 with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.6) as face_detection:
@@ -32,15 +34,15 @@ with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence
                 h = int(bboxC.height * ih)
 
                 # Expand the face box a bit
-                x1, y1 = max(x - 40, 0), max(y - 40, 0)
-                x2, y2 = min(x + w + 40, iw), min(y + h + 40, ih)
+                x1, y1 = max(x - 0, 0), max(y - 0, 0)
+                x2, y2 = min(x + w + 0, iw), min(y + h + 0, ih)
 
                 # Draw white box on mask where face is
                 mask[y1:y2, x1:x2] = 255
 
         # Blend background and face
         mask_3d = cv2.merge([mask, mask, mask])
-        output = np.where(mask_3d == 255, frame, blurred_frame)
+        output = np.where(mask_3d == 255, frame, bg_image)
 
         cv2.imshow("Shayan Smart Camera Filter", output)
         if cv2.waitKey(1) & 0xFF == 27:
